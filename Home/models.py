9c7fa9a727ec16ad55e59b626b42e7f12014a828
faisalPdev <mohdfaisal1459@gmail.com>
2024-02-23 21:31:03 +0530

    Banners & Sliders in Home Page

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+# ------------------slider----------------------
+
+class Slider(models.Model):
+    DEAL_TYPE=[
+        ('Hot Deal','Hot Deal'),
+        ('New Arrivals','New Arrivals'),
+        ('Best Sellers','Best Sellers'),
+        ('New Deal','New Deal'),
+    ]
+
+    title=models.CharField(max_length=255)
+    image=models.ImageField(upload_to= 'images/slider.jpg')
+    discount=models.PositiveIntegerField(default=0,null=True)
+    deal=models.CharField(choices=DEAL_TYPE,max_length=255)
+
+    def __str__(self):
+        return self.title[:50]
+
+class Banner(models.Model):
+    DEAL_TYPE=[
+        ('Hot Deal','Hot Deal'),
+        ('New Arrivals','New Arrivals'),
+        ('Best Sellers','Best Sellers'),
+        ('New Deal','New Deal'),
+    ]
+
+    title=models.CharField(max_length=255)
+    image=models.ImageField(upload_to= 'images/Banner.jpg')
+    discount=models.PositiveIntegerField(default=0,null=True)
+    deal=models.CharField(choices=DEAL_TYPE,max_length=255)
+
+    def __str__(self):
+        return self.title[:50]
+
+class Subbanner_1(models.Model):
+    title=models.CharField(max_length=255)
+    image=models.ImageField(upload_to= 'images/SubBanner_1.jpg')
+    free_shipping_from=models.PositiveIntegerField(default=0,null=True)
+
+    def __str__(self):
+        return self.title[:50]
--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from Home.models import *
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    slider=Slider.objects.all()[:3]
+    banner=Banner.objects.all()[:1]
+    subbanner=Subbanner_1.objects.all()[:2]
+    context={
+        'slider':slider,
+        'banner':banner,
+        'subbanner':subbanner,
+    }
+    return render(request,'home.html',context)
